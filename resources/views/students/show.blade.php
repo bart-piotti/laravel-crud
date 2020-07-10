18e9c18a01f2e58803d1f3abd0c4330fd714e0cd
Bartolomeo Piotti <piotti.bartolomeo@gmail.com>
2020-07-10 15:50:44 +0200

    edit from show

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -9,4 +9,9 @@
         <li>Email: {{ $data->email }} </li>
         <li>Registration Number:  {{ $data->registration_number }} </li>
     </ul>
+    <div class="container">
+        <div class="row" style="display: flex;justify-content: center;">
+            <a href="{{ route('students.edit', ['student' => $data->id ]) }}" class="btn btn-warning" style="color:white;">Edit</a>
+        </div>
+    </div>
 @endsection
